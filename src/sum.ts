dedd5da2fb8722d7966f1eaaf962e9204eb49352
artem subbotin <subbota1991@gmail.com>
2024-04-02 20:27:35 +0300

    task sum end

--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,31 @@
-const sum = () => {};
+const sum = (...args: any[]) => {
+  /*   if (typeof args !== 'object') {
+    throw new Error('INVALID_ARGUMENT');
+  }
+ */
+  if (args.length < 2) {
+    throw new Error('INVALID_ARGUMENTS_COUNT');
+  }
+
+  let result = 0;
+  for (let i = 0; i < args.length; i++) {
+    if (typeof args[i] !== 'number') {
+      throw new Error('INVALID_ARGUMENT');
+    }
+    result += args[i];
+  }
+  return result;
+};
 
 export default sum;
+/* function sum(...args: number[]): number {
+       
+  let result = 0;
+  for (let i = 0; i < args.length; i++) {
+      result+= args[i];
+  }
+  return result;
+}
+const numbers = [1, 3, 5, 7, 9] as const;
+
+console.log(num); // 25 */
