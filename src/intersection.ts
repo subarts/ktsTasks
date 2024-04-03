7f403c366a1beaaf2e7ec9b1f89e5e253fc1c766
artem subbotin <subbota1991@gmail.com>
2024-04-03 14:21:52 +0300

    pow,intersection

--- src/intersection.ts
+++ src/intersection.ts
@@ -1,3 +1,35 @@
-const intersection = () => {};
+const intersection = (...args) => {
+  if (args.length < 2) {
+    throw new Error('INVALID_ARGUMENTS_COUNT');
+  }
+
+  const arr1 = args[0];
+  const arr2 = args[1];
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    throw new Error('INVALID_ARGUMENT');
+  }
+
+  if (arr1.length == 0 || arr2.length == 0) {
+    return [];
+  }
+
+  if (
+    !arr1.every((a) => typeof a === 'number') ||
+    !arr2.every((a) => typeof a === 'number')
+  ) {
+    throw new Error('INVALID_ELEMENT_IN_ARRAY');
+  }
+  let arr3: any[] = [];
+  for (let i = 0; i < arr1.length; i++) {
+    {
+      if (arr2.includes(arr1[i])) {
+        arr3.push(arr1[i]);
+      }
+    }
+  }
+  let arr4 = new Set(arr3);
+
+  return Array.from(arr4);
+};
 
 export default intersection;
--- src/pow.ts
+++ src/pow.ts
@@ -1,3 +1,39 @@
-const pow = () => {};
+const curry = (func) => {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function (...args2) {
+        return curried.apply(this, args.concat(args2));
+      };
+    }
+  };
+};
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('INVALID_ARGUMENT');
+  }
+  return a ** b;
+}
 
+const pow = curry(sum);
 export default pow;
+/* const curry=(func)=> {
+
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function(...args2) {
+        return curried.apply(this, args.concat(args2));
+      }
+    }
+  };
+
+}
+function sum(a, b) {
+  return a ** b;
+}
+
+let pow = curry(sum);
+ */
