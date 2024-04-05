c07d6ea8b297f0adbaf5ef3bd29df95a7558e9a6
artem subbotin <subbota1991@gmail.com>
2024-04-05 12:50:06 +0300

    end sort

--- src/sort.ts
+++ src/sort.ts
@@ -1,3 +1,19 @@
-const sort = () => {};
+const sort = (str) => {
+  if (typeof str !== 'string') {
+    throw new Error('INVALID_ARGUMENT');
+  }
+  str = str.toLowerCase();
+  if (str.length < 2) {
+    return str;
+  }
+  if (str.search(' ') == -1) {
+    return str.split('').sort().join('');
+  }
+  str = str.split(' ');
+  for (let i = 0; i < str.length; i++) {
+    str[i] = str[i].split('').sort().join('');
+  }
+  return str.sort((a, b) => a.length - b.length).join(' ');
+};
 
 export default sort;
